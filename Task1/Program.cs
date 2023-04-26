fc0c279f4b8d7ad4cf398120793d05cff5a7ee64
ArhyParh <zescoarty@gmail.com>
2023-04-26 05:19:06 +0300

    Решил 2 первые задачи

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+Console.WriteLine("Введите 2 числа: ");
+int a1 = int.Parse(Console.ReadLine()!);
+int b1 = int.Parse(Console.ReadLine()!);
+if (a1 == b1) { Console.WriteLine("Числа одинаковые!"); }
+else if (a1 > b1) { Console.WriteLine("Max = " + a1 + " Min = " + b1); }
+else { Console.WriteLine("Max = " + b1 + " Min = " + a1); }
---
+++ Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+Console.WriteLine("Введите 3 числа: ");
+int a1 = int.Parse(Console.ReadLine()!);
+int b1 = int.Parse(Console.ReadLine()!);
+int c1 = int.Parse(Console.ReadLine()!);
+int [] obj = {a1,b1,c1};
+Console.WriteLine("Max = "+ obj.Max());
+
+// Второй вариант, без функций Max массива, будет закоменчен. 
+// Console.WriteLine("Введите 3 числа: ");
+// int a1 = int.Parse(Console.ReadLine()!);
+// int b1 = int.Parse(Console.ReadLine()!);
+// int c1 = int.Parse(Console.ReadLine()!);
+// int m = a1;
+// if (m > b1) { m = a1; }
+// else { m = b1; }
+// if (m < c1) { m = c1; }
+//  Console.WriteLine("Max = " + m);
